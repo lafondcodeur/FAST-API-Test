b97b155ff4843900f6e45a63e3cc9beea63d08cc
MACK LAFOND <macklafonddeboumas@gmail.com>
2026-05-21 01:08:50 +0100

    Add issue routes and schemas

--- main.py
+++ main.py
@@ -1,28 +1,7 @@
 from fastapi import FastAPI
 
+from app.routes.issues import router as issues_router
+
 app = FastAPI()
 
-items = [
-    {"id":1, "name":"MACK LAFOND","profil":"Développeur"},
-    {"id":2, "name":"Maxina","profil":"Développeur Fullstack"}
-]
-
-@app.get("/health")
-def health_check():
-    return {"status":"ok"}
-
-@app.get("/items")
-def get_items():
-    return items
-
-@app.get("/item/{item_id}")
-def get_item(item_id:int):
-    for item in items:
-        if item["id"] == item_id:
-            return item
-    return {"error": "Item no found"}
-
-@app.post("/items")
-def create_item(item:dict):
-    items.append(item)
-    return item
+app.include_router(issues_router)
